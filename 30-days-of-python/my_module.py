e65ed7c68b1af481642ba745a6058c15cde062ba
anwin-sanju <dev.anwinsanju@gmail.com>
2026-07-15 15:43:21 +0530

    modules (notes)

---
+++ 30-days-of-python/my_module.py
@@ -0,0 +1,20 @@
+def generate_full_name(first_name, last_name):
+    return f"{first_name} {last_name}"
+
+def two_sum(num1, num2):
+    return num1 + num2
+
+gravity = 9.81
+person = {
+    'first_name': 'Hazel',
+    'last_name': 'Lestrange',
+    'gender': 'female',
+    'age': 22,
+    'marital_status': False,
+    'skills': ['HTML', 'CSS', 'JavaScript'],
+    'address': {
+        'country': 'India',
+        'city': 'Kannur',
+        'pincode': 560072
+    }
+}
